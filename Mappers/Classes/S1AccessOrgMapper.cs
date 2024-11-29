9a4555f7ad78e8d1ba8c26403e1640574bebfe55
Jay9011 <whdtjq5904@gmail.com>
2024-11-29 12:57:10 +0900

    feat. 다중 데이터베이스 접근에 의한 Setup 주입

--- Mappers/Classes/S1AccessOrgMapper.cs
+++ Mappers/Classes/S1AccessOrgMapper.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
 using CoreDAL;
-using CoreDAL.Classes;
+using CoreDAL.Configuration.Interface;
 using CoreDAL.Dapper;
 using CoreDAL.Models;
+using CoreDAL.Models.DALs;
 using OrgDAL.Entities;
 using OrgDAL.ParamClasses.SECU_WEB;
 
@@ -13,14 +14,28 @@ namespace OrgDAL.Mappers.Classes
 {
     public class S1AccessOrgMapper: IOrgMapper
     {
-        ICoreDAL dal = new S1AccessCoreDAL();
+        #region 의존 주입
+
+        private readonly IDatabaseSetup _db;
+
+        #endregion
+        
+        public S1AccessOrgMapper(IDatabaseSetup databaseSetup)
+        {
+            #region 의존 주입
+
+            _db = databaseSetup;
+
+            #endregion
+        }
+
 
         public async Task<List<OrgEntity>> GetAllOrg()
         {
             try
             {
                 // 프로시저 실행
-                SQLResult result = await dal.ExecuteProcedureAsync("SECU_ORG_SEL", new SECU_ORG_SEL(){ Type = SECU_ORG_SEL.Types.GetAll });
+                SQLResult result = await _db.DAL.ExecuteProcedureAsync(_db, "SECU_ORG_SEL", new SECU_ORG_SEL(){ Type = SECU_ORG_SEL.Types.GetAll });
 
                 // 실패했거나 결과가 없으면 null 반환
                 if (!result.IsSuccess || result.DataSet == null || result.DataSet.Tables.Count == 0)
